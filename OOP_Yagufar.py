e9122b2add00205bfd1cc94c5486503bf4e7375d
Evan <172231L@mymail.nyp.edu.sg>
2017-12-14 10:18:08 +0800

    evan updated 14/12

--- OOP_Yagufar.py
+++ OOP_Yagufar.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
-from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, PasswordField, validators
+from flask import Flask, render_template, request, url_for, redirect
+import firebase_admin
+from firebase_admin import credentials, db
+from Storage import Storage
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, PasswordField, validators, ValidationError,IntegerField
 app = Flask(__name__)
 
 cred = credentials.Certificate('cred/yagufar-bb205-firebase-adminsdk-p7ypj-a0ee653a2d.json')
@@ -12,11 +15,13 @@ root = db.reference()
 
 class StorageForm(Form):
     recipientName = StringField('Recipent Name: ',[validators.Length(min=1,max=100),validators.DataRequired()])
-    lockerId = StringField('Locker ID: ',[validators.Length(min=1,max=100),validators.DataRequired()])
+    lockerId = StringField('Locker ID: ',[validators.DataRequired()])
     dateofdelivery = StringField('Date Of Delivery: ',[validators.DataRequired()])
-    phonenumber = StringField('Phone Number: ',[validators.DataRequired()])
+    phonenumber = IntegerField('Phone Number: ',[validators.DataRequired()])
+    emailaddress = StringField('Email Address: ',[validators.DataRequired()])
 
-@app.route('/')
+
+@app.route('/home/')
 def home():
     return render_template('index.html')
 
@@ -26,7 +31,7 @@ def storage2():
 
 
 @app.route('/Storage/',  methods=['GET', 'POST'])
-def Storage():
+def storage_item():
     form = StorageForm(request.form)
     if request.method == 'POST' and form.validate():
         recipientName = form.recipientName.data

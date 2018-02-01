21695cf42497f7c78cc2e23b4e8953968be62a21
afiq ali <173473L@mymail.nyp.edu.sg>
2018-02-01 21:51:57 +0800

    added things to the repair pages

--- Repair.py
+++ Repair.py
@@ -1,16 +1,8 @@
 class Repair:
-    def __init__(self, service, location, date, time, quest):
-        self.__chooseService = service
-        self.__chooseLocation = location
+    def __init__(self, date, time, quest):
         self.__chooseDate = date
         self.__chooseTime = time
         self.__chooseQuest = quest
-
-    def get_chooseService(self):
-        return self.__chooseService
-
-    def get_chooseLocation(self):
-        return self.__chooseLocation
 
     def get_chooseDate(self):
         return self.__chooseDate
@@ -20,12 +12,6 @@ class Repair:
 
     def get_chooseQuest(self):
         return self.__chooseQuest
-
-    def set_chooseService(self, service):
-        self.__chooseService = service
-
-    def set_chooseLocation(self, location):
-        self.__chooseLocation = location
 
     def set_chooseDate(self, date):
         self.__chooseDate = date
